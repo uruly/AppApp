0435ff9579cff05063ba322f34c7e26631824d53
Reona Kubo <rk.uru007@gmail.com>
2017-11-22 17:31:41 +0900

    ShareExtension追加

---
+++ ShareFromAppStoreExtension/ShareViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ShareViewController.swift
+//  ShareFromAppStoreExtension
+//
+//  Created by 久保　玲於奈 on 2017/11/22.
+//  Copyright © 2017年 Reona Kubo. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        //ポストを無効にする条件を書く
+        
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        
+        print(self.extensionContext?.inputItems.count)
+        print(self.contentText)
+        print()
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
