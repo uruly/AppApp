e36d2dbe11cbe77c7555222383ad9f916e512951
Reona Kubo <rk.uru007@gmail.com>
2017-11-23 15:39:31 +0900

    AppRealmDataの保存

--- ShareFromAppStoreExtension/ShareViewController.swift
+++ ShareFromAppStoreExtension/ShareViewController.swift
@@ -102,6 +102,21 @@ class ShareViewController: SLComposeServiceViewController {
     override func didSelectPost() {
         let extensionItem: NSExtensionItem = self.extensionContext?.inputItems.first as! NSExtensionItem
         let itemProviders = extensionItem.attachments as! [NSItemProvider]
+        loadData(itemProviders: itemProviders) { (name, developer, id, url, image) in
+            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+            print("この中きたよ")
+            self.saveAppData(name: name, developer: developer, id: id, url: url, image: image, date: Date())
+        }
+        
+    }
+    
+    func loadData(itemProviders:[NSItemProvider],_ completion:@escaping (String,String,String,String,Data)->()){
+        var name:String?
+        var developer:String?
+        var id:String?
+        var url:String?
+        var image:Data?
+        //var date = Date()
         
         for itemProvider in itemProviders {
             print(itemProvider.registeredTypeIdentifiers)
@@ -111,10 +126,18 @@ class ShareViewController: SLComposeServiceViewController {
                 itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: {
                     (item, error) in
                     
-                    // item に url が入っている
-                    let itemNSURL: NSURL = item as! NSURL
-                    print("urlあるよ\(item)")
-                    // 行いたい処理を書く
+                    url = (item as? URL)!.absoluteString
+                    //print("url\(url)")
+                    
+                    //let urlText = url!.absoluteString
+                    if let idRange = url?.range(of: "id"),let endIndex = url?.index(of: "?"){
+                        id = String(url![idRange.lowerBound ..< endIndex])
+                        //print("id\(id)")
+                    }
+                    
+                    if name != nil && developer != nil && id != nil && url != nil && image != nil {
+                        completion(name!,developer!,id!,url!,image!)
+                    }
                 })
             }
             
@@ -124,9 +147,12 @@ class ShareViewController: SLComposeServiceViewController {
                     (item, error) in
                     
                     // item にUIImageが入っている
+                    let uiImage = item as! UIImage
+                    image = UIImagePNGRepresentation(uiImage)
                     
-                    
-                    // 行いたい処理を書く
+                    if name != nil && developer != nil && id != nil && url != nil && image != nil {
+                        completion(name!,developer!,id!,url!,image!)
+                    }
                 })
             }
             
@@ -135,16 +161,58 @@ class ShareViewController: SLComposeServiceViewController {
                 itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: {
                     (item, error) in
                     
-                    
-                    // item にUIImageが入っている
-                    print("plain-text\(item)")
-                    
-                    
-                    // 行いたい処理を書く
+                    let text = item as! String
+                    if text.contains("App名"){
+                        let appLabelRange = text.range(of:"App名: ")
+                        let developLabelRange = text.range(of:"、デベロッパ: ")
+                        
+                        //App名
+                        var nameString = text[..<developLabelRange!.lowerBound]
+                        nameString.removeSubrange(appLabelRange!)
+                        print("name\(nameString)")
+                        name = String(nameString)
+                        
+                        //デベロッパ名
+                        let developString = text[developLabelRange!.upperBound ..< text.endIndex]
+                        print("develop\(developString)")
+                        developer = String(developString)
+                        
+                        if name != nil && developer != nil && id != nil && url != nil && image != nil {
+                            completion(name!,developer!,id!,url!,image!)
+                        }
+                    }
                 })
             }
         }
+    }
 
+
+    
+    func saveAppData(name:String,developer:String,id:String,url:String,image:Data,date:Date){
+        var config = Realm.Configuration()
+        config.deleteRealmIfMigrationNeeded = false
+        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.xyz.uruly.appapp")!
+        config.fileURL = url.appendingPathComponent("db.realm")
+        
+        let appData = AppRealmData(value: ["name":name,
+                                           "developer":developer,
+                                           "id":id,
+                                           "url":url,
+                                           "image":image,
+                                           "date":date])
+        do{
+            let realm = try Realm(configuration: config)
+            do {
+                try realm.write {
+                    realm.add(appData,update:true)
+                }
+            }catch{
+                print("error\(error)")
+            }
+        }catch{
+            print(error)
+        }
+        
     }
 
     override func configurationItems() -> [Any]! {
