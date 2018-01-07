45ae6f20baa70b464535b64b06a2773ef441eff5
Reona Kubo <rk.uru007@gmail.com>
2018-01-07 21:43:29 +0900

    壁紙追加

--- AppApp/MainView/ContentView/BottomMenu/BackgroundImage/BackgroundImageView.swift
+++ AppApp/MainView/ContentView/BottomMenu/BackgroundImage/BackgroundImageView.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class BackgroundImageView: UICollectionView {
+    
+    var imageList:[UIImage] = []
+    var currentIndexPath:IndexPath = IndexPath(row:0,section:0)
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -21,33 +24,77 @@ class BackgroundImageView: UICollectionView {
     convenience init(frame: CGRect){
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width:50,height:50)
-        layout.sectionInset = UIEdgeInsetsMake(15,15,15,15)
-        layout.minimumLineSpacing = 0
+        layout.sectionInset = UIEdgeInsetsMake(15,15,15,0)
+        layout.minimumLineSpacing = 15
         layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
         self.init(frame: frame, collectionViewLayout: layout)
         
         self.register(UINib(nibName:"BackgroundImageCell",bundle:nil), forCellWithReuseIdentifier: "backImageSet")
+        self.register(BackgroundPlusCell.self, forCellWithReuseIdentifier: "plus")
         self.delegate = self
         self.dataSource = self
         self.backgroundColor = UIColor.white
         
     }
     
+    func changeBackgroundImage() {
+        if let basePageVC:BasePageViewController = findViewController() {
+            print("basepageVCあるよ")
+            if let baseVC:BaseViewController = basePageVC.viewControllers?.first as? BaseViewController {
+            }
+        }
+    }
 }
 
 extension BackgroundImageView: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+//            if currentIndexPath == indexPath {
+//                return
+//            }
+            if let cell:BackgroundColorViewCell = collectionView.cellForItem(at: indexPath) as? BackgroundColorViewCell {
+                cell.checkImageView.isHidden = false
+                //currentColor = colorList[indexPath.row]
+            }
+            if let previousCell:BackgroundColorViewCell = collectionView.cellForItem(at: currentIndexPath) as? BackgroundColorViewCell{
+                previousCell.checkImageView.isHidden = true
+                currentIndexPath = indexPath
+            }
+            changeBackgroundImage()
+        }else {
+            //カラーピッカーを表示
+        }
+    }
 }
 
 extension BackgroundImageView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        if section == 0 {
+            return imageList.count > 5 ? 5 : imageList.count
+        }else {
+            return 1
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "backImageSet", for: indexPath)
-        cell.contentView.backgroundColor = UIColor.green
-        return cell
+        if indexPath.section == 0 {
+            let cell:BackgroundImageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "backImageSet", for: indexPath) as! BackgroundImageCell
+            //cell.contentView.backgroundColor = colorList[indexPath.row]
+            if currentIndexPath == indexPath {
+                cell.checkImageView.isHidden = false
+            }else {
+                cell.checkImageView.isHidden = true
+            }
+            return cell
+        }else {
+            let cell:BackgroundPlusCell = collectionView.dequeueReusableCell(withReuseIdentifier: "plus", for: indexPath) as! BackgroundPlusCell
+            return cell
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
     }
     
     
