9ef537a708c5bd083139625855df30c35c3e1d8c
Reona Kubo <rk.uru007@gmail.com>
2018-01-11 13:33:35 +0900

    レシート情報をGoogleAnalyticsに送る

--- AppApp/AppDelegate.swift
+++ AppApp/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Realm
 import RealmSwift
+import StoreKit
 import GoogleAnalytics
 
 @UIApplicationMain
@@ -18,69 +19,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
-//        print(Bundle.main.appStoreReceiptURL)
-//        if let receiptUrl: URL = Bundle.main.appStoreReceiptURL {
-//            print("れしーとurlaruyo")
-//            if let receiptData: NSData = NSData(contentsOf: receiptUrl){
-//                print("レシートデータあるよ")
-//                let receiptBase64Str: String = receiptData.base64EncodedString(options:[])
-//                let requestContents: NSDictionary = ["receipt-data": receiptBase64Str] as NSDictionary
-//                print("ここ")
-//                do {
-//                    let requestData: NSData = try JSONSerialization.data(withJSONObject: requestContents, options: .prettyPrinted) as NSData
-//                    let sandboxUrl: NSURL = NSURL(string: "https://sandbox.itunes.apple.com/verifyReceipt")!
-//                    let request: NSMutableURLRequest = NSMutableURLRequest(url: sandboxUrl as URL)
-//
-//                    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"content-type")
-//                    request.timeoutInterval = 5.0
-//                    request.httpMethod = "POST"
-//                    request.httpBody = requestData as Data
-//
-//
-//                    let configuration = URLSessionConfiguration.default
-//                    let session = URLSession(configuration: configuration, delegate:nil, delegateQueue:OperationQueue.main)
-//
-//                    let task = session.dataTask(with: request as URLRequest, completionHandler: {
-//                        (data, response, error) -> Void in
-//                        do {
-//
-//                            let json = try JSONSerialization.jsonObject(with: data!, options:JSONSerialization.ReadingOptions.allowFragments ) as! NSArray
-//                            print(json)
-//
-//                        } catch {
-//                            print(error)
-//                            //エラー処理
-//                        }
-//
-//                    })
-//
-//                }catch {
-//                    print(error)
-//                }
-//            }else {
-//                print("取得できない")
-//            }
-//        }
-//
+        
         let receiptValidator = ReceiptValidator()
         let validationResult = receiptValidator.validateReceipt()
         switch validationResult {
         case .success(let receipt):
-            // Work with parsed receipt data. Possibilities might be...
-            // enable a feature of your app
-            // remove ads
-        // etc...
-            print(receipt)
+            let appVersion = receipt.appVersion ?? ""
+            let date = receipt.expirationDate
+            let origAppVersion = receipt.originalAppVersion ?? ""
+            GATrackingManager.sendEventTracking(category: "APP VERSION:\(appVersion)", action: "DATE:\(String(describing: date))", label: "ORIGINAL VERSION:\(origAppVersion)")
         case .error(let error):
             // Handle receipt validation failure. Possibilities might be...
             // use StoreKit to request a new receipt
             // enter a "grace period"
             // disable a feature of your app
             // etc...
+            GATrackingManager.sendEventTracking(category: "レシートないよ", action: "\(error)", label: "")
             print(error)
         }
-        
         
         //window?.rootViewController = DetailViewController()
         // GoogleAnalyticsの設定
