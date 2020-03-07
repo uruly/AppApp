9ef63acde5ec07071983fa64312fff7b6f57a7d2
uruly <rk.uru007@gmail.com>
2020-03-07 21:08:13 +0900

    :rocket: Add DatabaseManager method #23

--- AppApp/Scripts/Database/DatabaseManager.swift
+++ AppApp/Scripts/Database/DatabaseManager.swift
@@ -24,27 +24,94 @@ final class DatabaseManager {
     }
 
     private lazy var configuration: Realm.Configuration = {
-        var configuration = Realm.Configuration(schemaVersion: .schemaVersion)
-        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: .groupID)!
-        configuration.fileURL = url.appendingPathComponent("db.realm")
+        var configuration = Realm.Configuration(schemaVersion: .schemaVersion, migrationBlock: migrationBlock)
+        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: .groupID)
+        configuration.fileURL = url?.appendingPathComponent("db.realm")
 
         return configuration
+    }()
+
+    private lazy var migrationBlock: RealmSwift.MigrationBlock? = {
+        return .init { (migration, schema) in
+            // マイグレーション処理
+        }
     }()
 }
 
 extension DatabaseManager {
 
+    // MARK: - Fetch
+
     func object<T: Object>(_ type: T.Type, filter predicate: NSPredicate? = nil) -> T? {
-        let results: Results<T>
-        if let pre = predicate {
-            results = realm.objects(type).filter(pre)
+        var results: Results<T>
+        if let predicate = predicate {
+            results = realm.objects(type).filter(predicate)
         } else {
             results = realm.objects(type)
         }
 
-        guard !results.isEmpty else {
-            return nil
+        return results.isEmpty ? nil : results.first
+    }
+
+    func objects<T: Object>(_ type: T.Type, filter predicate: NSPredicate? = nil, sortBy propertiesSort: [String: Bool]? = nil) -> Results<T> {
+        var results: Results<T>
+        if let predicate = predicate {
+            results = realm.objects(type).filter(predicate)
+        } else {
+            results = realm.objects(type)
         }
-        return results.first
+
+        if let propertiesSort = propertiesSort {
+            for property in propertiesSort {
+                results = results.sorted(byKeyPath: property.0, ascending: property.1)
+            }
+        }
+        return results
+    }
+
+    // MARK: - Add
+
+    func addObject<T: Object>(_ object: T) {
+        do {
+            realm.beginWrite()
+            realm.add(object, update: .all)
+            try realm.commitWrite()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    func addObjects<T: RealmSwift.Object, S: Sequence>(_ objects: S) where S.Iterator.Element == T {
+        do {
+            realm.beginWrite()
+            for item in objects {
+                realm.add(item, update: .all)
+            }
+            try realm.commitWrite()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    // MARK: - Delete
+
+    func deleteObject<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func deleteObjects<T: RealmSwift.Object, S: Sequence>(_ objects: S) where S.Iterator.Element == T {
+        do {
+            try realm.write {
+                realm.delete(objects)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
