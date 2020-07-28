ea297055b9614f53ee5f428decfce3db07644c19
uruly <rk.uru007@gmail.com>
2020-07-28 21:13:53 +0900

    :rocket: Add HomeViewController #27

---
+++ AppApp/Scripts/UIViewController/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  AppApp
+//
+//  Created by Reona Kubo on 2020/07/28.
+//  Copyright © 2020 Reona Kubo. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
