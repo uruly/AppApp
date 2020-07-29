936fb566f11d080369d6cb61095ea6cce7c41125
uruly <rk.uru007@gmail.com>
2020-07-29 18:20:04 +0900

    :pick: Modify label list #28

--- ShareFromAppStoreExtension/LabelListTableViewController.swift
+++ ShareFromAppStoreExtension/LabelListTableViewController.swift
@@ -15,28 +15,27 @@ import RealmSwift
 
 final class LabelListTableViewController: UITableViewController {
 
-    var labels: [Label] = []
+    private let reuseIdentifier = "labelList"
+
+    private var labels: [Label] = []
 
     weak var delegate: LabelListTableViewControllerDelegate?
-
-    static var isUnwindCreate = false
 
     // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "labelList")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.allowsMultipleSelection = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        readLabelData()
-    }
 
-    func readLabelData() {
-        labels = Label.getAll()
-        tableView.reloadData()
+        readLabel { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
 }
 
@@ -56,18 +55,15 @@ extension LabelListTableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "labelList", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.contentView.backgroundColor = .systemBackground
+        cell.accessoryView?.backgroundColor = .systemBackground
 
         if indexPath.section == 1 {
             cell.textLabel?.text = "新しいラベルを作成"
             cell.accessoryType = .none
         } else {
             cell.textLabel?.text = labels[indexPath.row].name
-            //            if self.delegate.shareVC.labelList.contains(where: {$0.id == labels[indexPath.row].id}) {
-            //                cell.accessoryType = .checkmark
-            //            } else {
-            //                cell.accessoryType = .none
-            //            }
         }
 
         return cell
@@ -81,21 +77,34 @@ extension LabelListTableViewController {
         }
 
         let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+        cell?.isHighlighted = false
+    }
 
-        //        if self.delegate.shareVC.labelList.contains(where: {$0.id == list[indexPath.row].id}) {
-        //            //外す
-        //            cell?.accessoryType = .none
-        //            let index = self.delegate.shareVC.labelList.findIndex {$0.id == list[indexPath.row].id}
-        //            if index.count > 0 {
-        //                self.delegate.shareVC.labelList.remove(at: index[0])
-        //            }
-        //
-        //        } else {
-        //            //つける
-        //            cell?.accessoryType = .checkmark
-        //            self.delegate.shareVC.labelList.append(list[indexPath.row])
-        //        }
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        cell.accessoryType = .none
+    }
+}
 
-        cell?.isSelected = false
+// MARK: - Realm
+
+extension LabelListTableViewController {
+
+    private func readLabel(_ completion: (() -> Void)) {
+        labels = Label.getAll()
+        defer { completion() }
+        guard labels.isEmpty else { return }
+        // ALL labelを追加する
+        guard let colorData = try? NSKeyedArchiver.archivedData(withRootObject: UIColor.blue, requiringSecureCoding: false) else {
+            fatalError("Color Archived Error!!")
+        }
+        let allLabel = Label(id: UUID.init().uuidString, name: "ALL", color: colorData, order: 0, explain: "すべてのApp")
+        labels = [allLabel]
+        do {
+            try Label.add(allLabel)
+        } catch {
+            print("Error")
+        }
     }
 }
