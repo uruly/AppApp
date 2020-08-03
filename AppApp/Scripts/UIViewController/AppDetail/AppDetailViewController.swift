ed12efdb07987d65606421476c9771cb3c11cf67
uruly <rk.uru007@gmail.com>
2020-08-03 20:50:52 +0900

    :rocket: Add AppDetailViewController #33

---
+++ AppApp/Scripts/UIViewController/AppDetail/AppDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AppDetailViewController.swift
+//  AppApp
+//
+//  Created by Reona Kubo on 2020/08/03.
+//  Copyright © 2020 Reona Kubo. All rights reserved.
+//
+
+import UIKit
+
+final class AppDetailViewController: UIViewController {
+
+    @IBOutlet private weak var imageView: UIImageView!
+
+    let app: App
+
+    // MARK: - Initializer
+
+    init(app: App) {
+        self.app = app
+        super.init(nibName: R.nib.appDetailViewController.name, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let imageData = app.image {
+            imageView.image = UIImage(data: imageData)
+        }
+        setupChildren()
+    }
+
+    private func setupChildren() {
+
+    }
+
+}
