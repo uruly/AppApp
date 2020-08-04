87e7e93aebaf72da952d5fcca3ae94790a81beb1
uruly <rk.uru007@gmail.com>
2020-08-04 13:15:04 +0900

    :shower: Remove not used files #34

---
+++ AppApp/Scripts/Enumerations/ToolbarMode.swift
@@ -0,0 +1,14 @@
+//
+//  ToolbarMode.swift
+//  AppApp
+//
+//  Created by Reona Kubo on 2020/08/04.
+//  Copyright © 2020 Reona Kubo. All rights reserved.
+//
+
+import Foundation
+
+enum ToolbarMode {
+    case collect
+    case list
+}
